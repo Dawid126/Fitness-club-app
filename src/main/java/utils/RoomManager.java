d1efe6e600a2f0966fd522e3effda8e7b32a4c3b
Dawid_Bialka <dbialka@student.agh.edu.pl>
2020-11-30 19:53:22 +0100

    roommanager

---
+++ src/main/java/utils/RoomManager.java
@@ -0,0 +1,41 @@
+package utils;
+
+import model.Activity;
+import model.Room;
+import org.checkerframework.checker.units.qual.A;
+import persistance.IDataManager;
+
+import java.util.List;
+
+public class RoomManager {
+    private final IDataManager dataManager;
+
+    public RoomManager(IDataManager dataManager) {
+        this.dataManager = dataManager;
+    }
+
+    public List<Room> getRooms() {
+        return dataManager.loadRooms();
+    }
+
+    public boolean removeRoom(Room roomToRemove) {
+        List<Activity> activies = dataManager.loadActivities();
+        for(Activity activity: activies) {
+            if(activity.getRoom().equals(roomToRemove)); //TODO zwracanie listy activities ktore uzywaja dany room
+            return false;
+        }
+        dataManager.removeRoom(roomToRemove);
+        return true;
+    }
+
+    public boolean createRoom(int capacity) {
+        Room newRoom = new Room(capacity);
+        if(!dataManager.loadRooms().contains(newRoom)) {
+            dataManager.saveRoom(new Room(capacity));
+            return true;
+        }
+        return false;
+    }
+
+
+}
