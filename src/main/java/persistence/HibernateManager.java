8d30e982fa34fb9ff1a71668e4b3bb288cf6396f
Łukasz Pitrus <305396@student.agh.edu.pl>
2020-12-13 20:19:38 +0000

    SessionService

--- src/main/java/persistence/HibernateManager.java
+++ src/main/java/persistence/HibernateManager.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class HibernateManager implements IDataManager {
     @Override
     public void saveActivities(List<Activity> activities) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         for(Activity activity : activities) {
@@ -22,10 +23,12 @@ public class HibernateManager implements IDataManager {
             session.merge(activity);
         }
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void saveClients(List<Client> clients) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         for(Client client : clients) {
@@ -33,10 +36,12 @@ public class HibernateManager implements IDataManager {
             session.merge(client);
         }
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void saveHosts(List<Host> hosts) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         for(Host host : hosts) {
@@ -44,10 +49,12 @@ public class HibernateManager implements IDataManager {
             session.merge(host);
         }
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void saveUsers(List<User> users) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         for(User user : users) {
@@ -55,10 +62,12 @@ public class HibernateManager implements IDataManager {
             session.merge(user);
         }
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void saveRooms(List<Room> rooms) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         for(Room room : rooms) {
@@ -66,6 +75,7 @@ public class HibernateManager implements IDataManager {
             session.merge(room);
         }
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
@@ -105,158 +115,194 @@ public class HibernateManager implements IDataManager {
 
     @Override
     public void saveActivity(Activity activity) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.save(activity);
         session.merge(activity);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void saveClient(Client client) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.save(client);
         session.merge(client);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void saveHost(Host host) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.save(host);
         session.merge(host);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void saveUser(User user) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.save(user);
         session.merge(user);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void saveRoom(Room room) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.save(room);
         session.merge(room);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void saveProduct(Product product) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.save(product);
         session.merge(product);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void updateActivity(Activity activity) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.update(activity);
         session.merge(activity);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void updateClient(Client client) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.update(client);
         session.merge(client);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void updateHost(Host host) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.update(host);
         session.merge(host);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void updateUser(User user) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.update(user);
         session.merge(user);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void updateRoom(Room room) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.update(room);
         session.merge(room);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void updateProduct(Product product) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.update(product);
         session.merge(product);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void removeActivity(Activity activity) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.delete(activity);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void removeClient(Client client) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.delete(client);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void removeHost(Host host) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.delete(host);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void removeUser(User user) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.delete(user);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void removeRoom(Room room) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.delete(room);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
     public void removeProduct(Product product) {
+        SessionService.openSession();
         final Session session = SessionService.getSession();
         final Transaction tx = session.beginTransaction();
         session.delete(product);
         tx.commit();
+        SessionService.closeSession();
     }
 
     @Override
