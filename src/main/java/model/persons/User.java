aba3f499b6a06b73258c80a8efaef2dd43bdf2c2
Łukasz Pitrus <305396@student.agh.edu.pl>
2020-12-13 14:17:02 +0000

    Next one

--- src/main/java/model/persons/User.java
+++ src/main/java/model/persons/User.java
@@ -2,10 +2,19 @@ package model.persons;
 
 import enums.Role;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User extends AbstractPerson{
+
+    @Column(name = "password", nullable = false, length = 50)
     private String password;
+
+    @Column(name = "role", nullable = false, length = 50)
     private Role role;
 
+    public User () {}
     public User (String name, String surname, String email, Role role, String password) {
         super(name, surname, email);
         this.role = role;
