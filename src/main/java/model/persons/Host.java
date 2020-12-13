3940c23e5cac84cad9086e2a6eddaf5b01230a04
Dawid_Bialka <dbialka@student.agh.edu.pl>
2020-12-13 15:00:32 +0100

    host refactor

--- src/main/java/model/persons/Host.java
+++ src/main/java/model/persons/Host.java
@@ -11,11 +11,6 @@ import java.util.*;
 @Table(name = "host")
 public class Host extends AbstractPerson{
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
-    @Column(name = "id")
-    private int id;
-
     @OneToMany(mappedBy = "host")
     private List<Activity> activities;
 
@@ -28,25 +23,5 @@ public class Host extends AbstractPerson{
 
     public List<Activity> getActivities() {
         return activities;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Host host = (Host) o;
-
-        if (id != host.id) return false;
-        if(this.getName() != host.getName()) return false;
-        if(this.getSurname() != host.getSurname()) return false;
-        return this.getEmail() == host.getEmail();
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result;
-        return result;
     }
 }
